2e04b017b0b5f02c4ae0ab140876f101373476f5
EndritGashi20 <endrit.gash@student.uni-pr.edu>
2024-08-07 00:19:39 +0200

    start 2.1

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,17 +1,46 @@
 
+import { useContext } from "react";
+import { NavLink } from "react-router-dom";
+import { GlobalContext } from "../../context";
 
 
-export default function Navbar(){
-    return <nav className="flex justify-between items-center py-8 container mx-auto flex-col lg:flex-row gap-5 lg:gap-0">
-     <h2 className="text-2x1 font-semibold">FoodRecipe</h2>
-     <form>
-     <input
+export default function Navbar() {
+
+const {searchParam,setSearchParam} = useContext(GlobalContext)
+console.log(searchParam);
+  return (
+    <nav className="flex justify-between items-center py-8 container mx-auto flex-col lg:flex-row gap-5 lg:gap-0">
+      <h2 className="text-2xl font-semibold">
+        <NavLink to={"/"}>FoodRecipe</NavLink>
+      </h2>
+      <form >
+        <input
           type="text"
           name="search"
+         value={searchParam}
+         onChange={(event)=>setSearchParam(event.target.value)}
           placeholder="Enter Items..."
           className="bg-white/75 p-3 px-8 rounded-full outline-none lg:w-96 shadow-lg shadow-red-100 focus:shadow-red-200"
         />
-     </form>
-        
+      </form>
+      <ul className="flex gap-5">
+        <li>
+          <NavLink
+            to={"/"}
+            className="text-black hover:text-gray-700 duration-300"
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to={"/favorites"}
+            className="text-black hover:text-gray-700 duration-300"
+          >
+            favorites
+          </NavLink>
+        </li>
+      </ul>
     </nav>
+  );
 }
