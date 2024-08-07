bfbf3c64e5273198c499c531fc18a3ac65ca336c
EndritGashi20 <endrit.gash@student.uni-pr.edu>
2024-08-07 23:26:39 +0200

    fatching api and crreating home page

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -5,7 +5,29 @@ export const GlobalContext = createContext(null);
 
 
 export default function GlobalState({ children }) {
-    const [searchParam,setSearchParam]= useState('')
-
-    return <GlobalContext.Provider value={{searchParam,setSearchParam}}>{children}</GlobalContext.Provider>
+    const [searchParam,setSearchParam]= useState("");
+    const[loading,setloading]=useState(false);
+    const[recipeList,setRecipeList]=useState([]);
+         async function handleSubmit(event){
+            event.preventDefault();
+            try{
+             const res = await fetch(
+                `https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchParam}`
+              );
+              const data = await res.json(); 
+              if(data?.data?.recipes){
+                setRecipeList(data?.data?.recipes)
+                setloading(false)
+                setSearchParam('')
+              }
+         
+            }catch(e){
+          console.log(e);
+          setloading(true)
+          setSearchParam('')
+          
+            }
+         }
+         console.log(loading,recipeList);
+    return <GlobalContext.Provider value={{searchParam,loading,recipeList,setSearchParam,handleSubmit}}>{children}</GlobalContext.Provider>
 }
